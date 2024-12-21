8058add250da1629d76be45d9cfc7062e3a883ba
unknown <IamHendy>
2024-12-21 20:29:25 +0300

    task 4

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -34,5 +34,16 @@ export interface PostProps {
     website: string
     company: CompanyProps;
    }
+  
+   export interface PostData {
+    userId: number;
+    id?: number;
+    title: string;
+    body: string;
+  }
 
+  export interface PostModalProps {
+    onClose: () => void;
+    onSubmit: (post: PostData) => void;
+  }
 
